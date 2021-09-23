927bb866885f2e47e771823a90e9cb4b50f12f7d
Leonardo Kaminski Ferreira <leonardo.kaminski.ferreira@gmail.com>
2021-09-23 20:33:56 -0300

    [FEAT] Add basic project structure

---
+++ BrazeDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  BrazeDemo
+//
+//  Created by Leonardo Vinicius Kaminski Ferreira on 23/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        print("loaded")
+    }
+
+
+}
+
